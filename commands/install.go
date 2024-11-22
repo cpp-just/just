edcac2a1c107315f880e87eb9cd8837cf94f0ca5
LucasCzerny <czernylucas@gmail.com>
2024-11-22 22:55:46 +0100

    Seeing if the commands work

--- commands/install.go
+++ commands/install.go
@@ -1,7 +1,18 @@
 package commands
 
-import "fmt"
+import (
+	"fmt"
+	// "os/exec"
+)
 
-func Install() {
-	fmt.Println("Installing glfw...")	
+func Install(packages ...string) {
+	const basePath = "https://github.com/LucasCzerny/justpackage-"
+
+	for _, pkg := range packages {
+		packagePath := basePath + pkg
+
+		command := fmt.Sprintf("git submodule add %s packages/%s", packagePath, pkg)
+		fmt.Printf("Executing \"%s\"\n", command)
+		// exec.Command(command)
+	}
 }
--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 )
 
 func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Failed to get the cwd.")
+	}
+
+	fmt.Printf("cwd is %s. Press enter to continue.\n", cwd)
+	fmt.Scanln()
+
 	flag.Parse()
 
 	if (flag.NArg() == 0) {
@@ -19,13 +27,18 @@ func main() {
 	command := flag.Args()[0]
 
 	switch (command) {
-	case "install": {
-		commands.Install()
-	}
-	case "update": {
+	case "install":
+		commands.Install(flag.Args()[1:])
+
+	case "update":
 		// update()
+
+	default:
+		printUsage()
+		os.Exit(1)
 	}
-	}
+
+	fmt.Println("Thanks for using just!")
 }
 
 func printUsage() {
